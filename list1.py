41725e93bd96402840676e8403d4ce2b9336d227
ehanson94 <ehanson94@gmail.com>
2019-11-18 12:28:32 -0500

    finished list1.py and list2.py

--- list1.py
+++ list1.py
@@ -25,8 +25,11 @@
 
 
 def match_ends(words):
-    """Your code goes here.  Edit this docstring."""
-    return
+    count = 0
+    for word in words:
+        if len(word) >= 2 and word[0] == word[-1]:
+            count += 1
+    return count
 
 
 # B. front_x
@@ -37,8 +40,16 @@ def match_ends(words):
 # Hint: this can be done by making 2 lists and sorting each of them
 # before combining them.
 def front_x(words):
-    """Your code goes here.  Edit this docstring."""
-    return
+    xlist = []
+    regList = []
+    for word in words:
+        if word[0] == "x":
+            xlist.append(word)
+        else:
+            regList.append(word)
+    xlist.sort()
+    regList.sort()
+    return xlist + regList
 
 
 # C. sort_last
@@ -48,8 +59,11 @@ def front_x(words):
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
 def sort_last(tuples):
-    """Your code goes here.  Edit this docstring."""
-    return
+    def sortLast(val): 
+        return val[-1]  
+
+    tuples.sort(key = sortLast)
+    return tuples
 
 
 # Simple provided test() function used in main() to print
--- list2.py
+++ list2.py
@@ -18,8 +18,11 @@
 
 
 def remove_adjacent(nums):
-    """Your code goes here.  Edit this docstring."""
-    return
+    newList = []
+    for value in nums:
+        if len(newList) == 0 or value != newList[-1]:
+            newList.append(value)
+    return newList
 
 
 # E. Given two lists sorted in increasing order, create and return a merged
@@ -27,8 +30,16 @@ def remove_adjacent(nums):
 # The solution should work in "linear" time, making a single pass of both lists.
 # Hint: Don't use `sort` or `sorted` -- they are not linear time.
 def linear_merge(list1, list2):
-    """Your code goes here.  Edit this docstring."""
-    return
+    result = []
+    
+    while list1 and list2:
+        if list1[0] < list2[0]:
+           result.append(list1.pop(0))
+        else:
+           result.append(list2.pop(0))
+    result.extend(list1)
+    result.extend(list2)
+    return result
 
 
 # Simple provided test() function used in main() to print
